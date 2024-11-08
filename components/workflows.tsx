b37665fec87a6373ed28d64a99d119b6a2d39fd5
Amin Taheri <amintaheri90@gmail.com>
2024-11-08 12:43:54 -0330

    added know the team section

--- components/workflows.tsx
+++ components/workflows.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import WorflowImg01 from "@/public/images/workflow-01.png";
-import WorflowImg02 from "@/public/images/workflow-02.png";
+import WorflowImg01 from "@/public/images/abolfazl-2.jpg";
+import WorflowImg02 from "@/public/images/aaron.jpg";
 import WorflowImg03 from "@/public/images/workflow-03.png";
 import Spotlight from "@/components/spotlight";
 
@@ -13,20 +13,18 @@ export default function Workflows() {
           <div className="mx-auto max-w-3xl pb-12 text-center md:pb-20">
             <div className="inline-flex items-center gap-3 pb-3 before:h-px before:w-8 before:bg-gradient-to-r before:from-transparent before:to-red-200/50 after:h-px after:w-8 after:bg-gradient-to-l after:from-transparent after:to-red-200/50">
               <span className="inline-flex bg-gradient-to-r from-red-500 to-red-200 bg-clip-text text-transparent">
-                Tailored Workflows
+                {/* Tailored Workflows */}
               </span>
             </div>
             <h2 className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.red.200),theme(colors.gray.50),theme(colors.red.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-4 font-nacelle text-3xl font-semibold text-transparent md:text-4xl">
-              Map your product journey
+              Know the team!
             </h2>
             <p className="text-lg text-red-200/65">
-              Simple and elegant interface to start collaborating with your team
-              in minutes. It seamlessly integrates with your code and your
-              favorite programming languages.
+              Our collective experience in AI is more than a decade. Meat who is behind our products!
             </p>
           </div>
           {/* Spotlight items */}
-          <Spotlight className="group mx-auto grid max-w-sm items-start gap-6 lg:max-w-none lg:grid-cols-3">
+          <Spotlight className="group mx-auto grid max-w-sm items-start gap-6 lg:max-w-2xl lg:grid-cols-2">
             {/* Card 1 */}
             <a
               className="group/card relative h-full overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-red-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-red-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
@@ -52,7 +50,7 @@ export default function Workflows() {
                 </div>
                 {/* Image */}
                 <Image
-                  className="inline-flex"
+                  className="mx-auto inline-flex"
                   src={WorflowImg01}
                   width={350}
                   height={288}
@@ -63,13 +61,12 @@ export default function Workflows() {
                   <div className="mb-3">
                     <span className="btn-sm relative rounded-full bg-gray-800/40 px-2.5 py-0.5 text-xs font-normal before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.gray.700/.15),theme(colors.gray.700/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-gray-800/60">
                       <span className="bg-gradient-to-r from-red-500 to-red-200 bg-clip-text text-transparent">
-                        Built-in Tools
+                        Abolfazl -The great Kaan - Mohammadi
                       </span>
                     </span>
                   </div>
                   <p className="text-red-200/65">
-                    Streamline the product development flow with a content
-                    platform that's aligned across specs and insights.
+                    While persuing his PhD, his passion couldn't be limited to just doing a PhD. So he started MediSightAI to change healthcare systems.
                   </p>
                 </div>
               </div>
@@ -110,25 +107,24 @@ export default function Workflows() {
                   <div className="mb-3">
                     <span className="btn-sm relative rounded-full bg-gray-800/40 px-2.5 py-0.5 text-xs font-normal before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.gray.700/.15),theme(colors.gray.700/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-gray-800/60">
                       <span className="bg-gradient-to-r from-red-500 to-red-200 bg-clip-text text-transparent">
-                        Scale Instantly
+                        Aaron (Amin) Taheri
                       </span>
                     </span>
                   </div>
                   <p className="text-red-200/65">
-                    Streamline the product development flow with a content
-                    platform that's aligned across specs and insights.
+                    With a Master in CS, he touched all areas pertaining to AI. From computer vision to Natural langage processing. 
                   </p>
                 </div>
               </div>
             </a>
             {/* Card 3 */}
-            <a
+            {/* <a
               className="group/card relative h-full overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-red-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-red-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
               href="#0"
             >
-              <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-[#0A082A] after:absolute after:inset-0 after:bg-gradient-to-br after:from-gray-900/50 after:via-gray-800/25 after:to-gray-900/50">
+              <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-[#0A082A] after:absolute after:inset-0 after:bg-gradient-to-br after:from-gray-900/50 after:via-gray-800/25 after:to-gray-900/50"> */}
                 {/* Arrow */}
-                <div
+                {/* <div
                   className="absolute right-6 top-6 flex h-8 w-8 items-center justify-center rounded-full border border-gray-700/50 bg-gray-800/65 text-gray-200 opacity-0 transition-opacity group-hover/card:opacity-100"
                   aria-hidden="true"
                 >
@@ -143,17 +139,17 @@ export default function Workflows() {
                       d="m4.92 8-.787-.763 2.733-2.68H0V3.443h6.866L4.133.767 4.92 0 9 4 4.92 8Z"
                     />
                   </svg>
-                </div>
+                </div> */}
                 {/* Image */}
-                <Image
+                {/* <Image
                   className="inline-flex"
                   src={WorflowImg03}
                   width={350}
                   height={288}
                   alt="Workflow 03"
-                />
+                /> */}
                 {/* Content */}
-                <div className="p-6">
+                {/* <div className="p-6">
                   <div className="mb-3">
                     <span className="btn-sm relative rounded-full bg-gray-800/40 px-2.5 py-0.5 text-xs font-normal before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.gray.700/.15),theme(colors.gray.700/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-gray-800/60">
                       <span className="bg-gradient-to-r from-red-500 to-red-200 bg-clip-text text-transparent">
@@ -165,9 +161,9 @@ export default function Workflows() {
                     Streamline the product development flow with a content
                     platform that's aligned across specs and insights.
                   </p>
-                </div>
-              </div>
-            </a>
+                </div> */}
+              {/* </div> */}
+            {/* </a> */}
           </Spotlight>
         </div>
       </div>
