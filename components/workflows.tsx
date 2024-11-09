a1fa6e6c6f1f8d259fb41ce55377781101127a5b
Amin Taheri <amintaheri90@gmail.com>
2024-11-09 11:04:30 -0330

    fixed mobile view "meet the team" and cropped aaron's pic

--- components/workflows.tsx
+++ components/workflows.tsx
@@ -24,7 +24,7 @@ export default function Workflows() {
             </p>
           </div>
           {/* Spotlight items */}
-          <Spotlight className="group mx-auto grid max-w-sm items-start gap-6 lg:max-w-2xl lg:grid-cols-2">
+          <Spotlight className="group mx-auto grid max-w-xs items-start gap-6 lg:max-w-2xl lg:grid-cols-2">
             {/* Card 1 */}
             <a
               className="group/card relative h-full overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-red-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-red-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
