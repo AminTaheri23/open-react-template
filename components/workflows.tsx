eb63fcc6adedaae13f4ff1f1c56b98fb9f6faef8
Amin Taheri <amintaheri90@gmail.com>
2024-12-22 13:50:06 -0330

    added Michaela

--- components/workflows.tsx
+++ components/workflows.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import WorflowImg01 from "@/public/images/abolfazl-2.jpg";
 import WorflowImg02 from "@/public/images/aaron.jpg";
-import WorflowImg03 from "@/public/images/workflow-03.png";
+import MichaelaImage from "@/public/images/Michaela_Rose.jpg";
 import Spotlight from "@/components/spotlight";
 
 export default function Workflows() {
@@ -11,20 +11,20 @@ export default function Workflows() {
         <div className="pb-12 md:pb-20">
           {/* Section header */}
           <div className="mx-auto max-w-3xl pb-12 text-center md:pb-20">
-            <div className="inline-flex items-center gap-3 pb-3 before:h-px before:w-8 before:bg-gradient-to-r before:from-transparent before:to-red-200/50 after:h-px after:w-8 after:bg-gradient-to-l after:from-transparent after:to-red-200/50">
-              <span className="inline-flex bg-gradient-to-r from-red-500 to-red-200 bg-clip-text text-transparent">
-                {/* Tailored Workflows */}
-              </span>
-            </div>
-            <h2 className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.red.200),theme(colors.gray.50),theme(colors.red.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-4 font-nacelle text-3xl font-semibold text-transparent md:text-4xl">
-              Know the team!
-            </h2>
-            <p className="text-lg text-red-200/65">
-              Our collective experience in AI is more than a decade. Meat who is behind our products!
-            </p>
+        <div className="inline-flex items-center gap-3 pb-3 before:h-px before:w-8 before:bg-gradient-to-r before:from-transparent before:to-red-200/50 after:h-px after:w-8 after:bg-gradient-to-l after:from-transparent after:to-red-200/50">
+          <span className="inline-flex bg-gradient-to-r from-red-500 to-red-200 bg-clip-text text-transparent">
+            {/* Tailored Workflows */}
+          </span>
+        </div>
+        <h2 className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.red.200),theme(colors.gray.50),theme(colors.red.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-4 font-nacelle text-3xl font-semibold text-transparent md:text-4xl">
+          Know the team!
+        </h2>
+        <p className="text-lg text-red-200/65">
+          Our collective experience in AI is more than a decade. Meet who is behind our products!
+        </p>
           </div>
           {/* Spotlight items */}
-          <Spotlight className="group mx-auto grid max-w-xs items-start gap-6 lg:max-w-2xl lg:grid-cols-2">
+          <Spotlight className="group mx-auto grid max-w-xs items-start gap-6 lg:max-w-2xl lg:grid-cols-3">
             {/* Card 1 */}
             <a
               className="group/card relative h-full overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-red-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-red-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
@@ -60,9 +60,9 @@ export default function Workflows() {
                 <div className="p-6">
                   <div className="mb-3">
                     <span className="btn-sm relative rounded-full bg-gray-800/40 px-2.5 py-0.5 text-xs font-normal before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.gray.700/.15),theme(colors.gray.700/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-gray-800/60">
-                      <span className="bg-gradient-to-r from-red-500 to-red-200 bg-clip-text text-transparent">
-                        Abolfazl -The great Kaan - Mohammadi
-                      </span>
+                        <span className="bg-gradient-to-r from-red-500 to-red-200 bg-clip-text text-transparent">
+                        Kaan Mohammadi
+                        </span>
                     </span>
                   </div>
                   <p className="text-red-200/65">
@@ -114,6 +114,52 @@ export default function Workflows() {
                   <p className="text-red-200/65">
                     With a Master in CS, he touched all areas pertaining to AI. From computer vision to Natural langage processing. 
                   </p>
+                </div>
+              </div>
+            </a>
+            {/* Card 3 */}
+            <a
+              className="group/card relative h-full overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-red-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-red-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
+              href="#0"
+            >
+              <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-[#0A082A] after:absolute after:inset-0 after:bg-gradient-to-br after:from-gray-900/50 after:via-gray-800/25 after:to-gray-900/50">
+                {/* Arrow */}
+                <div
+                  className="absolute right-6 top-6 flex h-8 w-8 items-center justify-center rounded-full border border-gray-700/50 bg-gray-800/65 text-gray-200 opacity-0 transition-opacity group-hover/card:opacity-100"
+                  aria-hidden="true"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={9}
+                    height={8}
+                    fill="none"
+                  >
+                    <path
+                      fill="#F4F4F5"
+                      d="m4.92 8-.787-.763 2.733-2.68H0V3.443h6.866L4.133.767 4.92 0 9 4 4.92 8Z"
+                    />
+                  </svg>
+                </div>
+                {/* Image */}
+                <Image
+                  className="inline-flex"
+                  src={MichaelaImage}
+                  width={350}
+                  height={288}
+                  alt="Michaela Rose"
+                />
+                {/* Content */}
+                <div className="p-6">
+                  <div className="mb-3">
+                    <span className="btn-sm relative rounded-full bg-gray-800/40 px-2.5 py-0.5 text-xs font-normal before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.gray.700/.15),theme(colors.gray.700/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-gray-800/60">
+                      <span className="bg-gradient-to-r from-red-500 to-red-200 bg-clip-text text-transparent">
+                        Michaela Rose
+                      </span>
+                    </span>
+                  </div>
+                    <p className="text-red-200/65">
+                    With a bachelor’s degree in nursing and a certificate in business, she is passionate about technology and its applications in healthcare innovation.
+                    </p>
                 </div>
               </div>
             </a>
