7ea2091716224942e4a50f033c517b890642d669
Amin Taheri <amintaheri90@gmail.com>
2025-01-05 19:49:24 -0330

    updated the main page and added video thumbnail and video

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -12,46 +12,21 @@ export default function HeroHome() {
           {/* Section header */}
           <div className="pb-12 text-center md:pb-20">
             <h1
-              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.red.200),theme(colors.gray.50),theme(colors.red.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
+              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.red.200),theme(colors.gray.50),theme(colors.red.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-7xl font-semibold text-transparent md:text-7xl"
               data-aos="fade-up"
             >
-              Introducing PRAISE
+              STRIDE
             </h1>
-            <h1
-              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.red.200),theme(colors.gray.50),theme(colors.red.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
-              data-aos="fade-up"
-            >
-              Patient Remote Assessment and Instant Screening Enginge 
-            </h1>
+            
             <div className="mx-auto max-w-3xl">
               <p
                 className="mb-8 text-xl text-red-200/65"
                 data-aos="fade-up"
                 data-aos-delay={200}
               >
-                PRAISE is our newest product that enables paitients to get screening with AI.
+                Stride helps people who want to improve their mobility and rehab experience through engaging exercise games verb.
               </p>
-              <h1
-                className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.red.200),theme(colors.gray.50),theme(colors.red.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-xl font-semibold text-transparent md:text-xl"
-                data-aos="fade-up"
-              >
-              
-                Share your thoughts on emergency room care—your input helps improve quality, access, and satisfaction! 
-              </h1>
               <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
-                <div data-aos="fade-up" data-aos-delay={400}>
-                  <a
-                    className="btn group mb-4 w-full bg-gradient-to-t from-red-600 to-red-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
-                    href="https://forms.gle/sN5a1WK6WRQZtqyG7"
-                  >
-                    <span className="relative inline-flex items-center">
-                      ER experience survey
-                      <span className="ml-1 tracking-normal text-white/50 transition-transform group-hover:translate-x-0.5">
-                        -&gt;
-                      </span>
-                    </span>
-                  </a>
-                </div>
                 <div data-aos="fade-up" data-aos-delay={600}>
                   <a
                     className="btn relative w-full bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%] sm:ml-4 sm:w-auto"
